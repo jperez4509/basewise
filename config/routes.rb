c71839710ed550b8bf88d9e527b26936fb73851f
dphaener <dphaener@gmail.com>
2015-02-25 00:24:53 -0800

    Added styling for projects page Added top nav Added font-awesome icons

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # Registration
+  get "register" => "registrations#new", as: "new_registration"
+  post "register" => "registrations#create", as: "create_registration"
+
+  # Projects
   resources :projects
+
+  # Users
+  resources :users
 end
